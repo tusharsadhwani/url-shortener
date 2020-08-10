85805ccaf50c7e01d572cbc22255e17698c9f683
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-08-10 13:34:33 +0530

    Add python script

---
+++ main.py
@@ -0,0 +1,28 @@
+"""URL Shortener Hack"""
+import json
+import os
+import shutil
+
+
+def main():
+    """Main Function"""
+    html = "<html><script>window.location.replace('{url}');</script></html>"
+
+    with open('links.json') as f:
+        links = json.load(f)
+
+    shutil.rmtree('dist', ignore_errors=True)
+    os.mkdir('dist')
+
+    for link in links:
+        html_document = html.format(url=link['url'])
+
+        linkname = link['name']
+        file_path = f'dist/{linkname}.html'
+
+        with open(file_path, 'w') as f:
+            f.write(html_document)
+
+
+if __name__ == "__main__":
+    main()
